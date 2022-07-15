6c56cb8ba67c0ea788678d2faec4618cbf696fa8
chiahsing24 <chiahsingtony@gmail.com>
2022-07-14 22:25:41 -0700

    Create kafka topic

---
+++ src/main/java/com/codingjx/sbkafka/config/KafkaTopicConfig.java
@@ -0,0 +1,17 @@
+package com.codingjx.sbkafka.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic javaguidesTopic() {
+        return TopicBuilder.name("javaguide")
+                .build();
+    }
+}
